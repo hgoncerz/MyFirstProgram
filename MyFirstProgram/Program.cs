7306c66e770eb3717b518cae531cb9cd55c4b3b8
hgoncerz <80000293+hgoncerz@users.noreply.github.com>
2022-08-06 07:16:13 +0200

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello");
+            Console.WriteLine("Hello!");
+
+            //This is a comment
+            /*
+             siema
+            sdfasd
+            asdf
+             */
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
