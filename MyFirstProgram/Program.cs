9be708e1e731f6d3e6111d41b42a8ddd0265798e
hgoncerz <80000293+hgoncerz@users.noreply.github.com>
2022-08-07 10:47:32 +0200

    class

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,13 +6,10 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            //string interpolation
 
-            String firstName = "Bro";
-            String lastName = "Code";
-            int age = 21;
-            Console.WriteLine($"Hello {firstName} {lastName}.");
-            Console.WriteLine($"You are {age,-10} years old.");
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
             Console.ReadLine();
 
         }
