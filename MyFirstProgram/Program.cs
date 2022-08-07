3d69cd71a974d072b15702def5f9ce43dbdb227f
hgoncerz <80000293+hgoncerz@users.noreply.github.com>
2022-08-07 10:12:27 +0200

    params and overloading

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,63 +6,22 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            do
-            {
 
+            double total;
+            total = CheckOut(3.99, 5.75, 15);
 
-                double num1 = 0;
-                double num2 = 0;
-                double result = 0;
-
-                Console.WriteLine("______________");
-                Console.WriteLine("Calculator Program");
-                Console.WriteLine("______________");
-
-                Console.Write("Enter number 1: ");
-                num1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter number 2: ");
-                num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Enter an option: ");
-                Console.WriteLine("+ : Add");
-                Console.WriteLine("- :Subtract");
-                Console.WriteLine("* : Multiply");
-                Console.WriteLine("/ : Divide");
-                Console.Write("Enter an option: ");
-
-                switch (Console.ReadLine())
-                {
-                    case "+":
-                        result = num1 + num2;
-                        Console.WriteLine($"Your resutl: {num1}+ {num2} = " + result);
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        Console.WriteLine($"Your resutl: {num1}- {num2} = " + result);
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        Console.WriteLine($"Your resutl: {num1}* {num2} = " + result);
-                        break;
-                    case "/":
-                        result = num1 / num2;
-                        Console.WriteLine($"Your resutl: {num1}/ {num2} = " + result);
-                        break;
-                    default:
-                        Console.WriteLine("That was not a valid option");
-                        break;
-                }
-                Console.WriteLine("Would you like to continue? (Y/N)");
-            } while (Console.ReadLine().ToUpper() == "Y");
-
-            Console.WriteLine("Bye!");
-
-
-
+            Console.WriteLine(total);
             Console.ReadKey();
 
-
+        }
+        static double CheckOut(params double[] prices)
+        {
+            double total = 0;
+            foreach(double price in prices)
+            {
+                total += price;
+            }
+            return total;
         }
     }
 }
