60e20e7f0dede1d8086fd7e499c396111ff58803
hgoncerz <hubertg489655@gmail.com>
2022-08-08 08:37:54 +0200

    objects

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,13 +6,38 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
+            Human human1 = new Human();
+            Human human2 = new Human();
 
-            Messages.Hello();
-            Messages.Waiting();
-            Messages.Bye();
+            human1.name = "Rick";
+            human1.age = 65;
+
+            human2.name = "Morty";
+            human2.age = 16;
+
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
+
             Console.ReadLine();
 
         }
     
+    }
+    class Human
+    {
+        public String name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + "is eating");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
     }
 }
