c9a5d3123f738e0443ccc78f9dabea6d9f6562ab
hgoncerz <hubertg489655@gmail.com>
2022-08-08 09:59:55 +0200

    ArrayOfObjects

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,46 +6,26 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            Car car = new Car();
-            Bicycle bicycle = new Bicycle();
-            Boat boat = new Boat();
+            Car[] garage1 = { new Car("Mustang"), new Car("Corvette"), new Car("Lambo") };
 
-            car.go();
-            Console.WriteLine(car.speed);
-            Console.WriteLine(car.wheels);
-
-            bicycle.go();
-            Console.WriteLine(bicycle.speed);
-            Console.WriteLine(bicycle.wheels);
-                        
-            boat.go();
-            Console.WriteLine(boat.speed);
-            Console.WriteLine(boat.wheels);
-
+        foreach(Car car in garage1)
+            {
+                Console.WriteLine(car.model);
+            }
 
             Console.ReadKey();
         }
     }
-    class Vehicle
-    {
-        public int speed = 0;
 
-        public void go()
+    class Car
+    {
+        public String model;
+
+        public Car(String model)
         {
-            Console.WriteLine("This vehicle is moving");
+            this.model = model;
         }
     }
-    class Car : Vehicle
-    {
-        public int wheels = 4;
-    }
-    class Bicycle: Vehicle
-    {
-        public int wheels = 2;
-    }
-    class Boat : Vehicle
-    {
-        public int wheels = 0;
-    }
+
 
 }
